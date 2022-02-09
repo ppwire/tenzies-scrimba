dd57db229861f79e4b740d02471348d0d69c0d22
Poom Muangthai <poom.mth@hotmail.com>
2022-02-09 09:27:39 +0700

    Project init create a simple card

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1 className="app-title text-karla">Tenzies</h1>
+      <h3 className="app-description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h3>
+      <button className="btn">
+        <h2 className="text-karla">Roll</h2>
+      </button>
+    </div>
+  )
+}
+
+export default App
